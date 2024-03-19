c9655be72a32e8e8412e761e3a4c088f88459ebc
Virtualize Brief <153381859+virtualizebrief@users.noreply.github.com>
2024-03-19 13:53:33 -0400

    Update imprivata_clean.sh

---
+++ igel/imprivata_clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo $(date +"%Y%m%d_%T")\n >> /run/Imprivata_BootCheck_Start.log
+
+sleep 300
+
+if [ -a /.imprivata_data/runtime/offline/Agent/FirstDomain.txt ]
+then
+	echo $(date +"%Y%m%d_%T")\n >>  /run/Imprivata_BootCheck_FileYes.log
+else
+	echo $(date +"%Y%m%d_%T")\n >>  /run/Imprivata_BootCheck_FileNo.log
+	ImprivataBootstrap -w
+	ImprivataBootstrap
+	reboot
+fi
+
+#EOF

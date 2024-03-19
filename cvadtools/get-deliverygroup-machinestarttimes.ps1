fd39ece9dac03985521492f0c2be118cb2304d69
Virtualize Brief <153381859+virtualizebrief@users.noreply.github.com>
2024-03-19 10:17:28 -0400

    Update get-deliverygroup-machinestarttimes.ps1

--- cvadtools/get-deliverygroup-machinestarttimes.ps1
+++ cvadtools/get-deliverygroup-machinestarttimes.ps1
@@ -1,5 +1,7 @@
 Clear-Host
-Write-Host "Gets a list of delivery group machines start time"
+write-host ""
+Write-Host "Powertools" -foregroundcolor cyan
+Write-Host "Gets a list of delivery group > machine start times"
 Write-Host ""
 
 $dgs = (Get-BrokerDesktopGroup).name
@@ -7,11 +9,17 @@ $LogDate = Get-Date -format "yyyyMMdd_hhmmss"
 
 ForEach ($dg in $dgs) {
 
-    $LogFile = "C:\Support\Reports\MachineStartTimes_$dg_$LogDate.csv"
-    Write-Host "Creating: $LogFile" -ForegroundColor Cyan
-    Get-BrokerMachine -DesktopGroupName $dg -RegistrationState Registered -MaxRecordCount 5000 |select hostedmachinename,@{n='Last Reboot';e={gcim win32_operatingsystem -Computer $_.hostedMachineName|select -expand lastboo*}} | Export-CSV -Path $LogFile
+    #$dg = Read-Host "Enter delivery group name"
+    $LogFile = "C:\Support\Reports\MachineStartTimes.$dg.$LogDate.csv"
+    Write-Host "Creating: " -NoNewLine
+    Write-Host "$LogFile..." -ForegroundColor Yellow -NoNewLine
+    
+        Get-BrokerMachine -DesktopGroupName $dg -RegistrationState Registered -MaxRecordCount 5000 | where-object {$_.name -ne "*kn*"} |select hostedmachinename,@{n='Last Reboot';e={gcim win32_operatingsystem -Computer $_.hostedMachineName|select -expand lastboo*}} | Export-CSV -Path $LogFile
+    
+    Write-Host "done!" -ForegroundColor Green
 
 }
 
+Write-Host ""
 Pause
 exit

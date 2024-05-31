6f244908399ec1ca02048d691cc27ab03e19d89e
Virtualize Brief <153381859+virtualizebrief@users.noreply.github.com>
2024-05-31 14:07:33 -0400

    Update custom-userlogin.sh

--- igel/custom-userlogin.sh
+++ igel/custom-userlogin.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
-
-# Variables
 USERNAME="myname"
 PASSWORD="mypassword"
 
-# Sleep for 20 secs
 sleep 20
 
-# Type the username
-xdotool type "$USERNAME"
-
-# Press Tab to move to the next input field
-xdotool key Tab
-
-# Type the password
-xdotool type "$PASSWORD"
-
-# Press Enter to submit the form
-xdotool key Return
-
-# Exit the script
+DISPLAY=:0 xdotool type "$USERNAME"
+DISPLAY=:0 xdotool key Tab
+DISPLAY=:0 xdotool type "$PASSWORD"
+DISPLAY=:0 xdotool key Return 
+#EOF

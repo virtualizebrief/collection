cbfcdd8e4c4ffe1fe856cb3bf0602c379814cd39
Virtualize Brief <virtualizebrief@gmail.com>
2024-11-19 17:39:25 -0500

    Create create-mcs-machines.ps1

---
+++ cvadtools/create-mcs-machines.ps1
@@ -0,0 +1,23 @@
+# set values
+$mc = 'machine-catalog-name'
+$dg = 'delivery-group-name'
+$count = '#'
+
+# get values
+$psUID = ((Get-ProvScheme -ProvisioningSchemeName $mc).ProvisioningSchemeUid).guid
+$mcUID = (get-brokercatalog -name $mc).uid
+
+# create new accounts and get new machine names
+New-AcctADAccount -IdentityPoolName $mc -Count $count
+$NewAccounts = (Get-AcctADAccount -IdentityPoolName $mc | Where-Object { $_.State -eq "Available" }).ADAccountName | Sort-Object
+$NewMachines = $NewAccounts.Replace('$','')
+
+# create machine, add to machine catalog, add to delivery group, power on
+ForEach ($NewMachine in $NewMachines) {
+
+    New-ProvVM -ADAccountName $NewMachines -ProvisioningSchemeUid $psUID
+    New-BrokerMachine -CatalogUid $NewMachine -MachineName $NewMachine
+    Add-BrokerMachine $NewMachine -DesktopGroup $dg
+    New-BrokerHostingPowerAction -MachineName $NewMachine -Action TurnOn
+
+}

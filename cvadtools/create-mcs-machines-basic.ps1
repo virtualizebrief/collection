9b6133c51e7ac985890399f500a0333ac4be505a
Virtualize Brief <virtualizebrief@gmail.com>
2025-01-27 11:25:44 -0500

    Update create-mcs-machines-basic.ps1

--- cvadtools/create-mcs-machines-basic.ps1
+++ cvadtools/create-mcs-machines-basic.ps1
@@ -1,7 +1,24 @@
+clear-host
+
 # set values
 $mc = 'machine-catalog-name'
 $dg = 'delivery-group-name'
 $count = '5'
+
+function timer {
+$duration = 300
+$endTime = (Get-Date).AddSeconds($duration)
+while ((Get-Date) -lt $endTime) {
+    # Calculate the percentage complete
+    $percentComplete = 100 - [int](((($endTime - (Get-Date)).TotalSeconds) / $duration) * 100)
+
+    # Display the progress bar
+    Write-Progress -Activity "Pausing for AD Accounts to register" -Status "Time remaining: $((($endTime - (Get-Date)).TotalSeconds).ToString('F0')) seconds" -PercentComplete $percentComplete
+
+    # Sleep for 1 second
+    Start-Sleep -Seconds 1
+    }
+}
 
 # get values for machine catalog, scheme & remove unused accounts
 $psUID = ((Get-ProvScheme -ProvisioningSchemeName $mc).ProvisioningSchemeUid).Guid
@@ -18,30 +35,22 @@ $Domains = $NewMachines -replace "\\.*$"
 $Domain = $Domains | Select-Object -First 1
     write-host ""
 
+timer
+
 # create machines
-    write-host "# Created machines" -ForegroundColor green
+    write-host "# Attempt 1 of 3" -ForegroundColor cyan
 $taskId = (New-ProvVM -ADAccountName $NewMachines -ProvisioningSchemeUid $psUID).taskId
+
+    write-host "Created machines: " -ForegroundColor green -NoNewline
 $CreatedVMs = $null #empty value to start with
 $CreatedVMs = ((Get-ProvTask -TaskID $taskId).CreatedVirtualMachines).VMName
-    $CreatedVMs
-    write-host ""
-    write-host "# Failed machines" -ForegroundColor red
+    ($CreatedVMs).count
+    write-host "Failed machines : " -ForegroundColor red -NoNewline
 $FailedVMs = $null #empty value to start with
 $FailedVMs = ((Get-ProvTask -TaskID $taskId).FailedVirtualMachines).VMName
-    $FailedVMs
-    write-host ""
+    ($FailedVMs).count
 
-<#
-# try failed machines again
-foreach ($FailedVM in $FailedVMs) {
-
-    New-ProvVM -ADAccountName lcmchealth\$NewMachines -ProvisioningSchemeUid $psUID | Select-Obect
-
-}
-#>
-
-
-If ($CreatedVMs -eq $null) {write-host "Total failure. No machines to create good-bye!" -ForegroundColor Red}
+If ($CreatedVMs -eq $null) {}
 Else {
 
 # add to machine catalog, delivery group, power on
@@ -55,5 +64,79 @@ foreach ($CreatedVM in $CreatedVMs) {
     }
 }
 
-pause
+if ((($FailedVMs).count) -gt 0) {
 
+#-Second try
+timer
+    write-host ""
+
+# create machines that failed, one more try!
+    write-host "# Attempt 2 of 3" -ForegroundColor cyan
+$FailedDomainVMs = $FailedVMs | ForEach-Object { "LCMCHEALTH\" + $_ }
+$taskId = (New-ProvVM -ADAccountName $FailedDomainVMs -ProvisioningSchemeUid $psUID).taskId
+
+    write-host "Created machines: " -ForegroundColor green -NoNewline
+$CreatedVMs = $null #empty value to start with
+$CreatedVMs = ((Get-ProvTask -TaskID $taskId).CreatedVirtualMachines).VMName
+    ($CreatedVMs).count
+
+    write-host "Failed machines : " -ForegroundColor red -NoNewline
+$FailedVMs = $null #empty value to start with
+$FailedVMs = ((Get-ProvTask -TaskID $taskId).FailedVirtualMachines).VMName
+    ($FailedVMs).count
+
+If ($CreatedVMs -eq $null) {}
+Else {
+
+# add to machine catalog, delivery group, power on
+foreach ($CreatedVM in $CreatedVMs) {
+    $CreatedVMNoDomain = $CreatedVM.Replace("$Domain\","")
+    $CreatedVMSid = ((get-adcomputer $CreatedVMNoDomain).sid).value
+    $CreatedVMAccount = Get-AcctADAccount -IdentityPoolName $mc -ADAccountSid $CreatedVMSid
+    $SendtoNull = New-BrokerMachine -CatalogUid $mcUID -MachineName $CreatedVMSid
+    $SendtoNull = Add-BrokerMachine -MachineName $CreatedVMSid -DesktopGroup $dg
+    $SendtoNull = New-BrokerHostingPowerAction -MachineName $CreatedVMSid -Action TurnOn
+    }
+}
+
+}
+
+if ((($FailedVMs).count) -gt 0) {
+#-Third try
+timer
+    write-host ""
+
+# create machines that failed, one more try!
+    write-host "# Attempt 3 of 3" -ForegroundColor cyan
+$FailedDomainVMs = $FailedVMs | ForEach-Object { "LCMCHEALTH\" + $_ }
+$taskId = (New-ProvVM -ADAccountName $FailedDomainVMs -ProvisioningSchemeUid $psUID).taskId
+
+    write-host "Created machines: " -ForegroundColor green -NoNewline
+$CreatedVMs = $null #empty value to start with
+$CreatedVMs = ((Get-ProvTask -TaskID $taskId).CreatedVirtualMachines).VMName
+    ($CreatedVMs).count
+
+    write-host "Failed machines : " -ForegroundColor red -NoNewline
+$FailedVMs = $null #empty value to start with
+$FailedVMs = ((Get-ProvTask -TaskID $taskId).FailedVirtualMachines).VMName
+    ($FailedVMs).count
+    write-host ""
+
+If ($CreatedVMs -eq $null) {}
+Else {
+
+# add to machine catalog, delivery group, power on
+foreach ($CreatedVM in $CreatedVMs) {
+    $CreatedVMNoDomain = $CreatedVM.Replace("$Domain\","")
+    $CreatedVMSid = ((get-adcomputer $CreatedVMNoDomain).sid).value
+    $CreatedVMAccount = Get-AcctADAccount -IdentityPoolName $mc -ADAccountSid $CreatedVMSid
+    $SendtoNull = New-BrokerMachine -CatalogUid $mcUID -MachineName $CreatedVMSid
+    $SendtoNull = Add-BrokerMachine -MachineName $CreatedVMSid -DesktopGroup $dg
+    $SendtoNull = New-BrokerHostingPowerAction -MachineName $CreatedVMSid -Action TurnOn
+    }
+}
+
+}
+
+write-host ""
+pause

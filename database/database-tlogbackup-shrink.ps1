a501a7bee5ccc9a1b7912c7b7ea24d9359396849
Michael Wood <153381859+virtualizebrief@users.noreply.github.com>
2025-03-06 09:02:12 -0500

    Update database-tlogbackup-shrink.ps1

--- database/database-tlogbackup-shrink.ps1
+++ database/database-tlogbackup-shrink.ps1
@@ -8,20 +8,39 @@ Quick way to shrink transaction logs on MS SQL Server.
 
 #>
 
-# Variables
-$sqlServer = "db-server"
-$backupFile = "C:\Support\Backup\tlog.bak"
+$getDate = Get-Date -format "yyyyMMdd-hhmmss"
+$logFile = "c:\support\log\database-tlogbackup-shrink-$getDate.log"
+
+function database-tlogbackup-shrink() {
+
+# Install-Module -Name SqlServer # if doesn't exist
+Import-Module SQLPS -Force
+
+clear-host
+$sqlServer = hostname
+# $dbs = "CitrixBlueSite"
+
 $dbs = @(
-"database1",
-"database2",
-"database3")
+"db1",
+"db2"
+)
 
-# Perform the work againest each database
 foreach ($db in $dbs) {
-write-host "Processing $db..."
 
-    If (Test-Path $backupFile -PathType Leaf) {Remove-Item -Path $backupFile -Force}   
-    Backup-SqlDatabase -ServerInstance $sqlServer -Database $db -BackupAction Log -BackupFile $backupFile -Verbose 
-    Invoke-Sqlcmd -ServerInstance $sqlServer -Query "DBCC SHRINKDATABASE('$db')" -Verbose
+    write-host "Processing $db..." -ForegroundColor yellow -nonewline
+        If (Test-Path 'C:\Support\Backup\tlog.bak' -PathType Leaf) {Remove-Item -Path 'C:\Support\Backup\tlog.bak' -Force}   
+        Backup-SqlDatabase -ServerInstance $sqlServer -Database $db -BackupAction Log -BackupFile 'C:\Support\Backup\tlog.bak' 
+    write-host "done!" -foregroundcolor green 
+
+    write-host "Resting before shrinking..." -ForegroundColor cyan -nonewline
+    start-sleep -s 10
+    Invoke-Sqlcmd -ServerInstance $sqlServer -QueryTimeout 0 -Query "DBCC SHRINKDATABASE('$db')"
+    write-host "done!" -foregroundcolor green 
+    write-host ""
 
 }
+
+}
+
+database-tlogbackup-shrink | Out-File -FilePath $logFile
+database-tlogbackup-shrink | Out-File -FilePath $logFile

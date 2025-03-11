e8485a3b5a2a84007f0f9c8ee666f903cf6801ac
Michael Wood <153381859+virtualizebrief@users.noreply.github.com>
2025-03-11 16:03:39 -0400

    Update get-vcenter-vms-inFolder.ps1

--- bonus/get-vcenter-vms-inFolder.ps1
+++ bonus/get-vcenter-vms-inFolder.ps1
@@ -10,12 +10,14 @@ $vUser     = Read-Host "Enter vCenter username"
 $vPassword = Read-Host "Enter vCenter password"  -AsSecureString
 $vPasswordPlain =[Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($vPassword))
 
-# Get VMs from soure folder & destination settings
-foreach ($sourceVC in $sourceVCs) {
+# Install powercli if not found
 if (-not (Get-Module -ListAvailable -Name VMware.PowerCLI)) {
     if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') {Set-PSRepository -Name PSGallery -InstallationPolicy Trusted}
     Install-Module -Name VMware.PowerCLI -Scope CurrentUser -Force -ErrorAction Stop
 }
+
+foreach ($sourceVC in $sourceVCs) {
+# Get VMs from soure folder
 $noShow  = Connect-VIServer -Server $sourceVC -User $vUser -Password $vPasswordPlain -ErrorAction Stop
 $vmNames = get-vm -server $sourceVC -location $sourceFolder | sort-object name
 $noShow  = Disconnect-VIServer -Server * -Force -Confirm:$false
@@ -25,7 +27,6 @@ write-host "$sourceVC" -ForegroundColor cyan
 write-host "Found VMs in folder $sourceFolder" -ForegroundColor yellow
 ($vmNames).name
 write-host ""
-
 }
 
 pause

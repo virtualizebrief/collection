e6549b89b4895789352f1dc748c0deb3cc87232b
Michael Wood <153381859+virtualizebrief@users.noreply.github.com>
2025-03-26 09:29:13 -0400

    Update restart-machines-waitbetween.ps1

--- bonus/restart-machines-waitbetween.ps1
+++ bonus/restart-machines-waitbetween.ps1
@@ -1,31 +1,44 @@
-set-location C:\Support\Apps\Powertools\CitrixApp\WindowsUpdate
-$computers = Get-Content .\computers.txt
+# Configurables
+$sleepTime = 200
+$machines = @(
+    "machine01.domain.com"
+    "machine02.domain.com"
+    "machine03.domain.com"
+    "machine04.domain.com"
+)
 
-Clear-Host
-Write-Host "Restart computers. 10 minutes between each restart."
-Write-Host ""
-Write-Host "Selected computers" -ForegroundColor Cyan
-$computers
-Write-Host "Enter to continue or CTRL+C to quit" -ForegroundColor Yellow
-Read-Host
-Write-Host "Results"
+# Clear the banner
+clear-host
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+write-host "# Reboot machines" -foregroundcolor cyan
 
-Foreach ($computer in $computers){
+# Run
+foreach ($machine in $machines) {
 
-    Write-Host "Sent restart to: " -ForegroundColor Cyan -NoNewLine
-    Write-Host "$computer" -ForegroundColor Yellow
-    Get-Date
+    $startTime = Get-Date -Format "hh:mm:ss tt" 
+        write-host "$startTime | Rebooting: " -NoNewline
+        write-host "$machine" -NoNewline -ForegroundColor Yellow
+        write-host "... " -NoNewline
+    restart-computer -computername $machine -force -wait
+        write-host "done!" -ForegroundColor Green    
 
-        Restart-Computer -ComputerName $computer -Force -Wait
+    Write-Progress -Activity "Sleeping" -Status "Starting..." -PercentComplete 0
 
-    Write-Host ""
+    for ($i = 0; $i -lt $sleepTime; $i += 1) {
+        Start-Sleep -Seconds $resolution
+        $percentComplete = ($i / $sleepTime) * 100
+        Write-Progress -Activity "Sleeping" -Status "Sleeping... ($i seconds / $sleepTime seconds)" -PercentComplete $percentComplete
+    }
 
-Start-Sleep -s 600
+    Write-Progress -Activity "Sleeping" -Status "Complete." -PercentComplete 100 -Completed
 
 }
 
-Write-Host ""
-Write-Host "done!"
+# Goodbye
+write-host ""
 pause
-exit
+
 

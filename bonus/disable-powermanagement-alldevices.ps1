d2c02663f99b433c6b45ae09d0b67574ecea5ef3
Michael Wood <153381859+virtualizebrief@users.noreply.github.com>
2025-04-08 18:00:57 -0400

    Create disable-powermanagement-alldevices.ps1

---
+++ bonus/disable-powermanagement-alldevices.ps1
@@ -0,0 +1,19 @@
+<#
+
+Removes check mark for allowing power management of devices on windows endpoints. They never sleep.
+
+#>
+
+# Dynamic power devices
+$powerMgmts = Get-CimInstance -ClassName MSPower_DeviceEnable -Namespace root/WMI
+
+# All USB devices
+$UsbDevices = Get-CimInstance -ClassName Win32_PnPEntity -Filter 'PNPClass = "USB"'
+
+$powerMgmts | ForEach-Object {
+    # Get the power management instance for this device, if there is one
+    $powerMgmt | Where-Object InstanceName -Like "*$($_.PNPDeviceID)*"
+} | Set-CimInstance -Property @{Enable = $true}
+
+# Results
+Get-CimInstance -ClassName MSPower_DeviceEnable -Namespace root/WMI

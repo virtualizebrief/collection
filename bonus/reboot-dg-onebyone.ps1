661ad53b9923f3b8fc0092b0ec4b00c49d5b9ecb
Michael Wood <153381859+virtualizebrief@users.noreply.github.com>
2025-11-13 16:17:48 -0500

    Create reboot-dg-onebyone.ps1

---
+++ bonus/reboot-dg-onebyone.ps1
@@ -0,0 +1,37 @@
+<#
+
+# Reboots each VDA in a Delivery Group one by one
+- Get machine names
+
+# Foreach machine
+- Maintenance on  | wait
+- Power off       | wait
+- Power on        | wait
+- Maintenance off | wait
+
+Repeat until all machines are completed. takes around 6 minutes per machine.
+
+#>
+
+# Your input please
+$dg = "my-deliverygroup"
+
+# Get list of machines
+$vdas = Get-BrokerDesktop -DesktopGroupName $dg | Select-Object MachineName
+
+# Maintenance-on, turn-off, turn-on, maintenance-off 
+ForEach ($vda in $vdas){
+
+    Set-BrokerMachineMaintenanceMode -InputObject $vda $true
+    start-sleep 15
+
+    New-BrokerHostingPowerAction -Action TurnOff -MachineName $vda
+    start-sleep 100
+
+    New-BrokerHostingPowerAction -Action TurnOn -MachineName $vda
+    start-sleep 200
+
+    Set-BrokerMachineMaintenanceMode -InputObject $vda $false
+    start-sleep 15
+
+}
